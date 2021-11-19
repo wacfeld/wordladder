8311068e6347c17da12c1e4b83ea8f505f89323b
wacfeld wang <wacfeldwang333@gmail.com>
2021-11-19 01:24:34 -0500

    made it BFS

--- main.c
+++ main.c
@@ -13,19 +13,66 @@
 #include <stdlib.h>
 #include <string.h>
 //#include <time.h>
+#define MAXLEN 2000
 #define putd(x) printf(#x ": %d\n", x)
 
-char dict[2000][4];
+char dict[MAXLEN][4];
 int dlen = 0;
-int inuse[2000];
+// int inuse[MAXLEN];
+int back[MAXLEN] = {[0 ... MAXLEN-1] = -1}; // start all with null backrefs
+
+int queue[MAXLEN]; // circular queue
+int qbeg, qend;
 
 int from;
 int to;
 
-int stack[2000];
-int slen = 0;
+// int stack[MAXLEN];
+// int slen = 0;
 
-int adj(char *a, char *b)
+
+void enqueue(int e)
+{
+    queue[qbeg++] = e;
+    qbeg %= MAXLEN;
+}
+
+int dequeue()
+{
+    int e = queue[qend++];
+    qend %= MAXLEN;
+    return e;
+}
+
+
+int findword(char *a)
+{
+    int lo = 0, hi = dlen;
+    int i;
+
+    i = (lo + hi)/2;
+    do
+    {
+        int c = strcmp(a, dict[i]);
+        if(c == 0)
+        {
+            return i;
+        }
+        else if(c < 0)
+        {
+            hi = i;
+        }
+        else
+        {
+            lo = i+1;
+        }
+        i = (lo+hi)/2;
+    } while(lo != hi);
+
+    return -1;
+}
+
+int isadj(char *a, char *b)
 {
     if(strlen(a) != strlen(b))
         return 0;
@@ -38,64 +85,108 @@ int adj(char *a, char *b)
     return c == 1;
 }
 
-void pstack()
+void putchain(int i) // print a chain of backreferences
 {
-    printf("found one of length %d\n", slen);
-    if(slen >= 20)
+    do
     {
-        return;
-    }
-    for(int i = 0; i < slen; i++)
-    {
-        printf("%s ", dict[stack[i]]);
-    }
-    puts("\n");
+        printf("%s <- ", dict[i]);
+        i = back[i];
+    } while(i != -1);
 }
 
-void climb(int f, int t)
+void bfsearch()
 {
-    putd(slen);
-    stack[slen++] = f;
-    if(f == t)
-    {
-        pstack();
-        slen--;
-        return;
-    }
 
-    inuse[f] = 1;
+    enqueue(from); // put start in queue
+    back[from] = -1; // end the chain of backreferences
 
-    for(int i = 0; i < dlen; i++)
+    while(qbeg != qend) // while there are still viable nodes to search
     {
-        if(inuse[i])
-            continue;
-        if(adj(dict[i], dict[f]))
+        int i = dequeue();
+        if(strcmp(dict[i], dict[to]) == 0) // found the target word
         {
-            climb(i,t);
+            putchain(i);
+        }
+        else
+        {
+            for(int j = 0; j < dlen; j++)
+            {
+                if(back[j] == -1)
+                {
+                    continue;
+                }
+                if(isadj(dict[i], dict[j]))
+                {
+                    enqueue(j);
+                    back[j] = i;
+                }
+            }
         }
     }
 
-    inuse[f] = 0;
-    slen--;
 }
+
+// void pstack()
+// {
+//     printf("found one of length %d\n", slen);
+//     if(slen >= 20)
+//     {
+//         return;
+//     }
+//     for(int i = 0; i < slen; i++)
+//     {
+//         printf("%s ", dict[stack[i]]);
+//     }
+//     puts("\n");
+// }
+
+// void climb(int f, int t)
+// {
+//     // putd(slen);
+//     stack[slen++] = f;
+//     if(f == t)
+//     {
+//         pstack();
+//         slen--;
+//         return;
+//     }
+
+//     inuse[f] = 1;
+
+//     for(int i = 0; i < dlen; i++)
+//     {
+//         if(inuse[i])
+//             continue;
+//         if(adj(dict[i], dict[f]))
+//         {
+//             climb(i,t);
+//         }
+//     }
+
+//     inuse[f] = 0;
+//     slen--;
+// }
+
+
 
 int main(int argc, char **argv)
 {
+    // read in dict
     putd(argc);
     while(scanf("%s", dict[dlen++]) != EOF) ;
+    // dict is already sorted
 
-    for(int i = 0; i < dlen; i++)
-    {
-        if(!strcmp(dict[i], argv[1]))
-            from = i;
-    }
+    putd(1);
+    // find start and end words
+    from = findword(argv[1]);
+    to   = findword(argv[2]);
 
-    for(int i = 0; i < dlen; i++)
-    {
-        if(!strcmp(dict[i], argv[2]))
-            to = i;
-    }
+    puts(argv[1]);
+    puts(argv[2]);
 
-    climb(from, to);
+    putd(from);
+    putd(to);
+
+
 
 }
